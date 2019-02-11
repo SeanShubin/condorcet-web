8eada6d74968f0b378e4f4854aab619bee84b5b5
Sean Shubin <sshubin@cj.com>
2019-02-11 10:18:32 -0800

    removed dead code

--- src/Main.kt
+++ src/Main.kt
@@ -2,11 +2,6 @@ import kotlin.browser.document
 import kotlin.browser.window
 
 fun main() {
-    val h1 = document.createElement("h1")
-    h1.textContent = "Hello, world!"
-    val body = document.body!!
-    body.appendChild(h1)
-
     val reactor = CondorcetReactor()
     val environment = Environment(document, window)
     val components = CondorcetComponents
